62a312323f5c37e2775473cc3f6ff421f3d0df76
Pacifique Ingabire <pacifiqueingabire222@gmail.com>
2026-06-28 15:38:36 +0200

    feat: add institutions API placeholder for NLA/RRA/RDB integration

---
+++ app/api/auth/institutions/route.js
@@ -0,0 +1,6 @@
+// TODO: Integrate with Rwandan institutions (NLA, RRA, RDB, credit bureaus)
+// This route will handle verified data retrieval when institution access is approved
+
+export async function POST(request) {
+  return Response.json({ message: 'Institution integration coming soon' }, { status: 501 })
+}
